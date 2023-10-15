5f49c0d8766f1b28559e6362458c2a132e493e59
AzureKn1ght <philsonnah@msn.com>
2023-10-15 16:26:57 +0800

    fixed bugs, added claiming

--- alpha365.js
+++ alpha365.js
@@ -21,7 +21,7 @@ const TKN_ABI = require("./tokenABI");
 // Import the environment variables and contract addresses
 const VAULT_ADR = "0xF5c27FaD680Ea584dc9973F80920D74aCc1290af";
 const USDC_ADR = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
-const PCS_ADR = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const PCS_ADR = "0x8cBd19dE48E307a5F9fE8414Fc5247E5B17DeA48"; //note: changed to Alpha Swap
 const TKN_ADR = "0x5d75675E9DA82524B5DfBe3439Fe3a6E29f2b967";
 const RPC_URL = process.env.BSC_RPC;
 
@@ -100,6 +100,7 @@ const connect = async (wallet) => {
   connection.wallet = new ethers.Wallet(wallet.key, connection.provider);
   connection.router = new ethers.Contract(PCS_ADR, SWAP_ABI, connection.wallet);
   connection.token = new ethers.Contract(TKN_ADR, TKN_ABI, connection.wallet);
+  connection.USDC = new ethers.Contract(USDC_ADR, TKN_ABI, connection.wallet);
   connection.vault = new ethers.Contract(
     VAULT_ADR,
     VAULT_ABI,
@@ -163,7 +164,7 @@ const ALPHACompound = async () => {
   // loop through for each wallet
   for (const wallet of wallets) {
     if (claimTime) {
-      const action = claim(wallet);
+      const action = pure_claim(wallet);
       report.mode = "claim";
       promises.push(action);
     } else {
@@ -262,6 +263,7 @@ const payGas = async (wallet, tries = 1.0) => {
         payment: true,
         tries: tries,
         url: url,
+        claim: await claimGas(vault_id, wallet),
       };
 
       // return status
@@ -288,6 +290,76 @@ const payGas = async (wallet, tries = 1.0) => {
     // failed, retrying again...
     console.log(`retrying(${tries})...`);
     return await payGas(wallet, ++tries);
+  }
+};
+
+// Claim Gas to Vault
+const claimGas = async (vault_id, wallet, tries = 1.0) => {
+  const w = wallet.address.slice(0, 5) + "..." + wallet.address.slice(-6);
+  try {
+    console.log(`- Wallet ${wallet["index"]} -`);
+    console.log("Claiming Gas...");
+
+    // connection using the current wallet
+    const connection = await connect(wallet);
+    const nonce = await connection.provider.getTransactionCount(wallet.address);
+    const m = Math.floor((60 * 60000) / tries);
+    const vault_id = Number(wallet.vaultID);
+
+    // set custom gasPrice
+    const overrideOptions = {
+      nonce: nonce,
+      gasLimit: Math.floor(2000000 / tries),
+      gasPrice: ethers.utils.parseUnits(tries.toString(), "gwei"),
+    };
+
+    // call the action function and await the results
+    const result = await connection.vault.claimVaultGas(
+      vault_id,
+      overrideOptions
+    );
+    const receipt = await connection.provider.waitForTransaction(
+      result.hash,
+      1,
+      m
+    );
+    const url = "https://bscscan.com/tx/" + result.hash;
+
+    // succeeded
+    if (receipt) {
+      console.log(`Claim${wallet["index"]}: success`);
+      const success = {
+        index: wallet.index,
+        wallet: w,
+        gas_claim: true,
+        tries: tries,
+        url: url,
+      };
+
+      // return status
+      return success;
+    }
+  } catch (error) {
+    console.log(`Wallet${wallet["index"]}: failed!`);
+    console.error(error);
+
+    // max 5 tries
+    if (tries > 5) {
+      // failed
+      const failure = {
+        index: wallet.index,
+        wallet: w,
+        claimGas: false,
+        error: error,
+      };
+
+      // return status
+      return failure;
+    }
+
+    // failed, retrying again...
+    console.log(`retrying(${tries})...`);
+    return await claimGas(vault_id, wallet, ++tries);
   }
 };
 
@@ -546,6 +618,165 @@ const claim = async (wallet, tries = 1.0) => {
   }
 };
 
+// Claim and Sell Tokens
+const pure_claim = async (wallet, tries = 1.0) => {
+  const w = wallet.address.slice(0, 5) + "..." + wallet.address.slice(-6);
+  try {
+    console.log(`- Wallet ${wallet["index"]} -`);
+    console.log("Claiming...");
+
+    // connection using the current wallet
+    const connection = await connect(wallet);
+    const nonce = await connection.provider.getTransactionCount(wallet.address);
+    const m = Math.floor((60 * 60000) / tries);
+    const vault_id = Number(wallet.vaultID);
+
+    // set custom gasPrice
+    const overrideOptions = {
+      nonce: nonce,
+      gasLimit: Math.floor(2000000 / tries),
+      gasPrice: ethers.utils.parseUnits(tries.toString(), "gwei"),
+    };
+
+    // call the action function and await the results
+    const result = await connection.vault.claim(vault_id, overrideOptions);
+    const receipt = await connection.provider.waitForTransaction(
+      result.hash,
+      1,
+      m
+    );
+    const url = "https://bscscan.com/tx/" + result.hash;
+
+    // succeeded
+    if (receipt) {
+      // get the principal balance currently in the vault
+      const v = await connection.vault.allInvestment(vault_id);
+      const balance = ethers.utils.formatEther(v.amountTokenInvested);
+      const b = await connection.provider.getBalance(wallet.address);
+      console.log(`Wallet${wallet["index"]}: success`);
+      console.log(`Vault Balance: ${balance} ALPHA`);
+      const bal = ethers.utils.formatEther(b);
+      const date = Number(v.lastTimeCompound);
+      const gas = Number(v.timePaidVaultGas);
+      const sell = await sellTokens(wallet);
+
+      const success = {
+        index: wallet.index,
+        wallet: w,
+        BNB: bal,
+        balance: balance,
+        claim: true,
+        timestamp: date,
+        gastime: gas,
+        tries: tries,
+        url: url,
+        sell: sell,
+      };
+
+      // return status
+      return success;
+    }
+  } catch (error) {
+    console.log(`Wallet${wallet["index"]}: failed!`);
+    console.error(error);
+
+    // max 5 tries
+    if (tries > 5) {
+      // failed
+      const failure = {
+        index: wallet.index,
+        wallet: w,
+        pure_claim: false,
+        error: error,
+      };
+
+      // return status
+      return failure;
+    }
+
+    // failed, retrying again...
+    console.log(`retrying(${tries})...`);
+    return await pure_claim(wallet, ++tries);
+  }
+};
+
+// Sell Individual Wallet
+const sellTokens = async (wallet, tries = 1.0) => {
+  const w = wallet.address.slice(0, 5) + "..." + wallet.address.slice(-6);
+  try {
+    console.log(`- Wallet ${wallet["index"]} -`);
+    console.log("Selling...");
+
+    // connection using the current wallet
+    const connection = await connect(wallet);
+
+    // form the inputs for calling the swap
+    const sellAmount = await connection.token.balanceOf(wallet.address);
+    console.log("Selling Off: " + sellAmount.toString());
+    const maxAmt = sellAmount.mul(100) || Number.MAX_SAFE_INTEGER;
+
+    // call selling function and await the results
+    const result = await connection.router.sellAlphaForBUSD(sellAmount, maxAmt);
+    const url = "https://bscscan.com/tx/" + result.hash;
+    const receipt = await result.wait();
+
+    // succeeded
+    if (receipt) {
+      const v = await connection.vault.getTotalCompoundedAndClaim(
+        wallet.vaultID
+      );
+      console.log(`SELL${wallet["index"]}: success`);
+      const sold = ethers.utils.formatEther(sellAmount);
+
+      //get USDC balance of wallet
+      let bal = await connection.USDC.balanceOf(wallet.address);
+      bal = ethers.utils.formatEther(bal);
+
+      const success = {
+        index: wallet.index,
+        wallet: w,
+        sellAmount: sold,
+        USDC: bal,
+        vault: {
+          vaultCompounded: ethers.utils.formatEther(v[0]),
+          vaultClaimed: ethers.utils.formatEther(v[1]),
+          AR: v[2],
+          ARJail: v[3],
+          unClaimedReward: ethers.utils.formatEther(v[4]),
+          vaultGas: ethers.utils.formatEther(v[5]),
+        },
+        sell: true,
+        tries: tries,
+        url: url,
+      };
+
+      // return status
+      return success;
+    }
+  } catch (error) {
+    console.log(`Wallet${wallet["index"]}: failed!`);
+    console.error(error);
+
+    // max 5 tries
+    if (tries > 5) {
+      // failed
+      const failure = {
+        index: wallet.index,
+        wallet: w,
+        sellTokens: false,
+        error: error,
+      };
+
+      // return status
+      return failure;
+    }
+
+    // failed, retrying again...
+    console.log(`retrying(${tries})...`);
+    return await sellTokens(wallet, ++tries);
+  }
+};
+
 // Job Scheduler Function
 const scheduleNext = async (nextDate) => {
   // set next job to be 24hrs 1min from now
@@ -575,24 +806,26 @@ const storeData = async () => {
 // Get ALPHA Price Function
 const alphaPrice = async () => {
   const url =
-    "https://www.dextools.io/app/en/bnb/pair-explorer/0xaaa6a5aff2d6fa6d26d4ba18d76a4d9f3ac75aea";
+    "https://bscscan.com/token/0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d?a=0x8cbd19de48e307a5f9fe8414fc5247e5b17dea48#tokenAnalytics";
   try {
     // just initialize connection
     const wallets = initWallets(1);
     const connection = await connect(wallets[0]);
+    const one = ethers.utils.parseEther("1.0");
 
     // get the price from PancakSwap
-    const rawPrice = await connection.router.getAmountsOut(
-      ethers.utils.parseEther("1"),
-      [TKN_ADR, USDC_ADR]
-    );
-
-    let price = ethers.utils.formatEther(rawPrice[1]);
+    const rawPrice = await connection.router.getTokenValue(one);
+    let price = ethers.utils.formatEther(rawPrice);
     price = Number(price).toFixed(4);
-    return { ALPHA: price, dextools: url };
+
+    //get USDC balance of contract
+    let bal = await connection.USDC.balanceOf(PCS_ADR);
+    bal = ethers.utils.formatEther(bal);
+
+    return { ALPHA: price, balance: bal, bscscan: url };
   } catch (error) {
     console.error(error);
-    return { dextools: url };
+    return { bscscan: url };
   }
 };
 
